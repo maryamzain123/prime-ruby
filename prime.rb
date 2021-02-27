8bb9123a6f328053d68e674e604d79ba9fc0f5ea
maryamzain123 <smaryam.zain@gmail.com>
2021-02-27 09:02:58 -0600

    commit tree c3bd563da2a6390fae3de4c060922d75453a075b

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(number)
+    if number <= 1
+        return false
+    else
+        (2...number).to_a.all? do |factor|
+            number % factor != 0
+        end
+    end
+
+end
